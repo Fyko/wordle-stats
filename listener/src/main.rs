7d3dc3cc07aa418f0cd9f65c38532d7219fcc51d
Carter Himmel <carter@elhnet.net>
2022-03-15 12:44:24 -0600

    feat: linting

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -17,76 +17,76 @@ use crate::metrics::DARK_MODE;
 
 #[tokio::main]
 async fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+	env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
-    let token = egg_mode::Token::Access {
-        consumer: egg_mode::KeyPair::new(
-            var("TWITTER_CONSUMER_KEY").unwrap(),
-            var("TWITTER_CONSUMER_SECRET").unwrap(),
-        ),
-        access: egg_mode::KeyPair::new(
-            var("TWITTER_ACCESS_KEY").unwrap(),
-            var("TWITTER_ACCESS_SECRET").unwrap(),
-        ),
-    };
+	let token = egg_mode::Token::Access {
+		consumer: egg_mode::KeyPair::new(
+			var("TWITTER_CONSUMER_KEY").unwrap(),
+			var("TWITTER_CONSUMER_SECRET").unwrap(),
+		),
+		access: egg_mode::KeyPair::new(
+			var("TWITTER_ACCESS_KEY").unwrap(),
+			var("TWITTER_ACCESS_SECRET").unwrap(),
+		),
+	};
 
-    let server = HttpServer::new(|| {
-        App::new()
-            .service(metrics::serve_metrics)
-            .wrap(Logger::default())
-    })
-    .bind(("0.0.0.0", 2489))
-    .unwrap()
-    .run();
-    tokio::spawn(server);
+	let server = HttpServer::new(|| {
+		App::new()
+			.service(metrics::serve_metrics)
+			.wrap(Logger::default())
+	})
+	.bind(("0.0.0.0", 2489))
+	.unwrap()
+	.run();
+	tokio::spawn(server);
 
-    let stream = egg_mode::stream::filter()
-        .track(&["Wordle"])
-        .language(&["en"])
-        .start(&token)
-        .try_for_each(|m| {
-            if let StreamMessage::Tweet(tweet) = m {
-                TWEET_COUNT.inc();
+	let stream = egg_mode::stream::filter()
+		.track(&["Wordle"])
+		.language(&["en"])
+		.start(&token)
+		.try_for_each(|m| {
+			if let StreamMessage::Tweet(tweet) = m {
+				TWEET_COUNT.inc();
 
-                let parsed = Wordle::try_from(tweet.text.as_str());
-                match parsed {
-                    Ok(game) => {
-                        info!("Parsed score for Day {}: {}/6", game.day, game.score);
+				let parsed = Wordle::try_from(tweet.text.as_str());
+				match parsed {
+					Ok(game) => {
+						info!("Parsed score for Day {}: {}/6", game.day, game.score);
 
-                        if game.hard {
-                            info!("Game was in hard mode");
-                            HARD_MODE.with_label_values(&[&game.day.to_string()]).inc();
-                        }
+						if game.hard {
+							info!("Game was in hard mode");
+							HARD_MODE.with_label_values(&[&game.day.to_string()]).inc();
+						}
 
-                        if game.guesses[0].contains(&GuessChar::Black) {
-                            info!("User is in dark mode!");
-                            DARK_MODE.with_label_values(&[&game.day.to_string()]).inc();
-                        }
+						if game.guesses[0].contains(&GuessChar::Black) {
+							info!("User is in dark mode!");
+							DARK_MODE.with_label_values(&[&game.day.to_string()]).inc();
+						}
 
-                        GAME_COUNTER_VEC
-                            .with_label_values(&[&game.day.to_string(), &game.score.to_string()])
-                            .inc();
-                    }
-                    Err(_) => {}
-                }
-            } else {
-                println!("{:?}", m);
-            }
-            futures::future::ok(())
-        });
+						GAME_COUNTER_VEC
+							.with_label_values(&[&game.day.to_string(), &game.score.to_string()])
+							.inc();
+					}
+					Err(_) => {}
+				}
+			} else {
+				println!("{:?}", m);
+			}
+			futures::future::ok(())
+		});
 
-    ctrlc::set_handler(move || {
-        let encoder = TextEncoder::new();
-        let metric_families = prometheus::gather();
-        let mut buffer = vec![];
-        encoder.encode(&metric_families, &mut buffer).unwrap();
-        println!("{}", String::from_utf8_lossy(&buffer));
-        std::process::exit(0);
-    })
-    .expect("Error setting Ctrl-C handler");
+	ctrlc::set_handler(move || {
+		let encoder = TextEncoder::new();
+		let metric_families = prometheus::gather();
+		let mut buffer = vec![];
+		encoder.encode(&metric_families, &mut buffer).unwrap();
+		println!("{}", String::from_utf8_lossy(&buffer));
+		std::process::exit(0);
+	})
+	.expect("Error setting Ctrl-C handler");
 
-    if let Err(e) = stream.await {
-        println!("Stream error: {}", e);
-        println!("Disconnected")
-    }
+	if let Err(e) = stream.await {
+		println!("Stream error: {}", e);
+		println!("Disconnected")
+	}
 }
--- listener/src/metrics.rs
+++ listener/src/metrics.rs
@@ -5,40 +5,40 @@ use prometheus::{register_counter, register_counter_vec, Counter, CounterVec};
 use prometheus::{Encoder, TextEncoder};
 
 lazy_static! {
-    pub static ref TWEET_COUNT: Counter = register_counter!(
-        "wordle_stats_tweet_count",
-        "Total amount of tweets the service has received."
-    )
-    .unwrap();
-    pub static ref HARD_MODE: CounterVec = register_counter_vec!(
-        "wordle_stats_hard_mode",
-        "Total amount of hard mode games parsed.",
-        &["game"]
-    )
-    .unwrap();
-    pub static ref DARK_MODE: CounterVec = register_counter_vec!(
-        "wordle_stats_dark_mode",
-        "Total amount of dark mode players.",
-        &["game"]
-    )
-    .unwrap();
-    pub static ref GAME_COUNTER_VEC: CounterVec = register_counter_vec!(
-        "wordle_stats_games",
-        "Wordle Game Statistics.",
-        &["game", "score"]
-    )
-    .unwrap();
+	pub static ref TWEET_COUNT: Counter = register_counter!(
+		"wordle_stats_tweet_count",
+		"Total amount of tweets the service has received."
+	)
+	.unwrap();
+	pub static ref HARD_MODE: CounterVec = register_counter_vec!(
+		"wordle_stats_hard_mode",
+		"Total amount of hard mode games parsed.",
+		&["game"]
+	)
+	.unwrap();
+	pub static ref DARK_MODE: CounterVec = register_counter_vec!(
+		"wordle_stats_dark_mode",
+		"Total amount of dark mode players.",
+		&["game"]
+	)
+	.unwrap();
+	pub static ref GAME_COUNTER_VEC: CounterVec = register_counter_vec!(
+		"wordle_stats_games",
+		"Wordle Game Statistics.",
+		&["game", "score"]
+	)
+	.unwrap();
 }
 
 #[get("/metrics")]
 pub async fn serve_metrics() -> impl Responder {
-    let encoder = TextEncoder::new();
+	let encoder = TextEncoder::new();
 
-    let metric_families = prometheus::gather();
-    let mut buffer = vec![];
-    encoder.encode(&metric_families, &mut buffer).unwrap();
+	let metric_families = prometheus::gather();
+	let mut buffer = vec![];
+	encoder.encode(&metric_families, &mut buffer).unwrap();
 
-    HttpResponse::Ok()
-        .append_header((CONTENT_TYPE, encoder.format_type()))
-        .body(buffer)
+	HttpResponse::Ok()
+		.append_header((CONTENT_TYPE, encoder.format_type()))
+		.body(buffer)
 }
